3ed96c9782ecab1476fa6917118900aa12ccfabc
“grsn-educa” <grsn.educa@gmail.com>
2021-09-30 19:03:33 -0300

    correco bug ao inserir reg no BD

--- src/main/java/br/com/alura/carteira/service/TransacaoService.java
+++ src/main/java/br/com/alura/carteira/service/TransacaoService.java
@@ -12,6 +12,7 @@ import br.com.alura.carteira.dto.TransacaoFormDto;
 import br.com.alura.carteira.modelo.Transacao;
 import br.com.alura.carteira.repository.TransacaoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class TransacaoService {
@@ -24,9 +25,11 @@ public class TransacaoService {
         List<Transacao> transacoes = transacaoRepository.findAll();
         return transacoes.stream().map(t -> modelMapper.map(t, TransacaoDto.class)).collect(Collectors.toList());
     }
-
+    
+    @Transactional  
     public void cadastrar(TransacaoFormDto dto) {
         Transacao transacao = modelMapper.map(dto, Transacao.class);
+        transacao.setId(null);
         transacaoRepository.save(transacao);
     }
 }

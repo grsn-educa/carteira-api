906360e31a737c9e1a722da0dd72cfa54e498690
“grsn-educa” <grsn.educa@gmail.com>
2021-09-30 04:13:00 -0300

    mapeamento das entidades Transacoes e Usuario

--- src/main/java/br/com/alura/carteira/modelo/Transacao.java
+++ src/main/java/br/com/alura/carteira/modelo/Transacao.java
@@ -2,24 +2,49 @@ package br.com.alura.carteira.modelo;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
-
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
-@Getter 
+@Getter
 @Setter
 @ToString(exclude = {"data", "quantidade", "tipo"})
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
+/* indica que è uma classe que mapeia uma tabela no banco de dados */
+@Table(name = "transacoes")
 public class Transacao {
-	
-	private String ticker;
-	private LocalDate data;
-	private BigDecimal preco;
-	private int quantidade;
-	private TipoTransacao tipo;
+
+    @Id //indica chave primaria
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String ticker;
+    private LocalDate data;
+    private BigDecimal preco;
+//  private int quantidade;
+    private Integer quantidade;
+    
+    @Enumerated(EnumType.STRING)
+    private TipoTransacao tipo;
+    
+    @ManyToOne
+//    @JoinColumn(name = "id_usuario")
+    private Usuario usuario;
 
 }
+
+/*
+    @Column(name = "nomeDaColuna") - em cima do atributo
+*/
--- src/main/java/br/com/alura/carteira/modelo/Usuario.java
+++ src/main/java/br/com/alura/carteira/modelo/Usuario.java
@@ -1,20 +1,30 @@
 package br.com.alura.carteira.modelo;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
-@Getter 
+@Getter
 @Setter
 @ToString(exclude = {"senha"})
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
+@Table(name = "usuarios")
 public class Usuario {
-	
-	private String nome;
-	private String login;
-	private String senha;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String login;
+    private String senha;
 
 }

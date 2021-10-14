7524e7b6b0c00036167e77e3ddd1250a421a20ed
“grsn-educa” <grsn.educa@gmail.com>
2021-10-14 08:18:22 -0300

    ajustes na classe LivroService

--- src/main/java/br/com/alura/carteira/service/TransacaoService.java
+++ src/main/java/br/com/alura/carteira/service/TransacaoService.java
@@ -3,7 +3,10 @@ package br.com.alura.carteira.service;
 import br.com.alura.carteira.dto.TransacaoDto;
 import br.com.alura.carteira.dto.TransacaoFormDto;
 import br.com.alura.carteira.modelo.Transacao;
+import br.com.alura.carteira.modelo.Usuario;
 import br.com.alura.carteira.repository.TransacaoRepository;
+import br.com.alura.carteira.repository.UsuarioRepository;
+import javax.persistence.EntityNotFoundException;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -16,23 +19,31 @@ public class TransacaoService {
 
     @Autowired
     private TransacaoRepository transacaoRepository;
-    
-//    @Autowired
-//    private UsuarioRepository usuarioRepository;
-    
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
     private ModelMapper modelMapper = new ModelMapper();
 
     public Page<TransacaoDto> listar(Pageable paginacao) {
         Page<Transacao> transacoes = transacaoRepository.findAll(paginacao);
         return transacoes.map(t -> modelMapper.map(t, TransacaoDto.class));
     }
-    
-    @Transactional  
+
+    @Transactional
     public TransacaoDto cadastrar(TransacaoFormDto dto) {
-//        throw new NullPointerException("teste");
-        Transacao transacao = modelMapper.map(dto, Transacao.class);
-        transacao.setId(null);
-        transacaoRepository.save(transacao);
-        return modelMapper.map(transacao, TransacaoDto.class);
+        Long idUsuario = dto.getUsuarioId();
+
+        try {
+            Usuario usuario = usuarioRepository.getById(idUsuario);
+            Transacao transacao = modelMapper.map(dto, Transacao.class);
+            transacao.setId(null);
+            transacao.setUsuario(usuario);
+            transacaoRepository.save(transacao);
+            return modelMapper.map(transacao, TransacaoDto.class);
+        } catch (EntityNotFoundException e) {
+            throw new IllegalArgumentException("usuario inexistente!");
+        }
+
     }
 }
--- src/test/java/br/com/alura/carteira/service/TransacaoServiceTest.java
+++ src/test/java/br/com/alura/carteira/service/TransacaoServiceTest.java
@@ -7,11 +7,14 @@ import br.com.alura.carteira.repository.TransacaoRepository;
 import br.com.alura.carteira.repository.UsuarioRepository;
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import javax.persistence.EntityNotFoundException;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
@@ -38,6 +41,8 @@ public class TransacaoServiceTest {
         );
 
         TransacaoDto dto = service.cadastrar(formDto);
+        
+//        Mockito.verify(transacaoRepository.save(Mockito.any()));
 
         assertEquals(formDto.getTicker(), dto.getTicker());
         assertEquals(formDto.getPreco(), dto.getPreco());
@@ -45,4 +50,18 @@ public class TransacaoServiceTest {
         assertEquals(formDto.getTipo(), dto.getTipo());
     }
 
+    @Test
+    void naoDeveriaCadastrarUmaTransacaoComUsuarioInexistente() {
+        TransacaoFormDto formDto = new TransacaoFormDto(
+                "ITSA4",
+                new BigDecimal("10.45"),
+                LocalDate.now(),
+                120,
+                TipoTransacao.COMPRA,
+                1l
+        );
+
+        Mockito.when(usuarioRepository.getById(formDto.getUsuarioId())).thenThrow(EntityNotFoundException.class);
+        assertThrows(IllegalArgumentException.class, () -> service.cadastrar(formDto));
+    }
 }
